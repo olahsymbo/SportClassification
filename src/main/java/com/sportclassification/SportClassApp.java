e6d30cab9a01179228228a4b5ad6083386ac5507
Olasimbo <olahsymbo@gmail.com>
2022-11-18 19:25:06 +0100

    set up base project

---
+++ src/main/java/com/sportclassification/SportClassApp.java
@@ -0,0 +1,13 @@
+package com.sportclassification;
+
+public class SportClassApp {
+
+    /**
+     * The main method of the application.
+     *
+     * @param args - command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a sport project");
+    }
+}
