65726b1466766b9a0b2fb53a717413ff8bfff9a7
Olasimbo <olahsymbo@gmail.com>
2022-12-03 17:09:20 +0100

    included CNN model

--- src/main/java/com/sportclassification/SportClassApp.java
+++ src/main/java/com/sportclassification/SportClassApp.java
@@ -3,16 +3,13 @@ package com.sportclassification;
 import com.sportclassification.data.DataLoader;
 import com.sportclassification.models.CNNModel;
 import org.nd4j.evaluation.classification.Evaluation;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.List;
 
 public class SportClassApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String data_path = "/src/main/java/com/sportclassification/dataset/train";
         DataLoader dl = new DataLoader(data_path);
         List<BufferedImage> allTrainImages = dl.loadImages();
